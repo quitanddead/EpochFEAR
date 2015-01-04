5c37c3058e789b46327beaf27cda75ee65e21935
trasher23 <trasher23@users.noreply.github.com>
2015-01-04 14:57:03 +0000

    Create spawn_zombie_herd.sql

---
+++ dayz_private_1.epoch.chernarus/FEAR/custom/spawn_zombie_herd.sql
@@ -0,0 +1,25 @@
+private["_genzombie","_playerpos","_agent","_zombieselectedpos"];
+
+ZombieType = ["zZombie_Base", "z_worker3", "z_worker2", "z_worker1", "z_villager1", "z_villager2", "z_villager3", "z_worker3", "z_worker2", "z_worker1", "z_villager1", "z_villager2", "z_villager3", "z_worker3", "z_worker2", "z_worker1", "z_villager1", "z_villager2", "z_villager3"];
+
+while {(!isDedicated) && (true)} do {
+
+	zombieHerd = false;
+	sleep 1;
+	waitUntil {zombieHerd};
+	
+	_playerpos = getposATL player;
+	
+	_i = 1;
+	for "_i" from 1 to 30 do
+	{
+		_genzombie = ZombieType call BIS_fnc_selectRandom;
+		_agent = createAgent [_genzombie, _playerpos, [], 40, "NONE"];
+		_zombieselectedpos = getPosATL _agent;
+		[_zombieselectedpos,_agent] execFSM "\z\addons\dayz_code\system\zombie_agent.fsm";
+	};
+	
+	sleep 0.5;
+	zombieHerd = false;
+	
+};
